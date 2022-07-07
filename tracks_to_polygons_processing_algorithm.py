bd39dd4b13c0a99e6aa2a5a7e6d690bf367df4dd
R <olegaruk@yandex.ru>
2022-07-07 14:58:07 +0300

    Испр. ош - добавл импорт модуля.

--- tracks_to_polygons_processing_algorithm.py
+++ tracks_to_polygons_processing_algorithm.py
@@ -6,6 +6,7 @@ from qgis.core import (QgsWkbTypes,
                        QgsFeatureSink,
                        QgsProcessing,
                        QgsProcessingAlgorithm,
+                       QgsProcessingException,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterFeatureSink)
 
